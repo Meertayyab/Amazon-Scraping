47378fcdfab85d089f0552329f285b2c3b72f20d
Meer Tayyab <137114744+Meertayyab@users.noreply.github.com>
2025-05-30 15:42:47 +0500

    New Changes

--- Backend/utils/memory.js
+++ Backend/utils/memory.js
@@ -1,6 +1,6 @@
 const logMemoryUsage=(label='') =>{
     const used = process.memoryUsage();
-    console.log(`[Memory ${label}]
+    console.log(`[Memory  ${label}]
      heapUsed: ${(used.heapUsed/1024/1024).toFixed(2)} MB`);
 }
 
